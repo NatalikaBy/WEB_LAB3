a62784a47e7915b3c85db8c6ac13f86bf8549133
Natali Boiko <59263928+NatalikaBy@users.noreply.github.com>
2020-12-01 00:39:55 +0300

    Add files via upload

---
+++ students.js
@@ -0,0 +1,86 @@
+var data = [
+  {
+    "id": 0,
+    "firstName": "Vyacheslau",
+    "surname": "Protosovickii",
+    "age": 18,
+    "score":  8.7
+  },
+  {
+    "id": 1,
+    "firstName": "Daniil",
+    "surname": "Privalov",
+    "age": 19,
+    "score":  3.8
+  },
+  {
+    "id": 2,
+    "firstName": "Daniil",
+    "surname": "Vorona",
+    "age": 21,
+    "score":  6.3
+  },
+  {
+    "id": 3,
+    "firstName": "Liliya",
+    "surname": "Nguen",
+    "age": 18,
+    "score": 7.3
+  },
+  {
+    "id": 4,
+    "firstName": "Genadiy",
+    "surname": "Malevich",
+    "age": 20,
+    "score": 8.9
+  },
+  {
+    "id": 5,
+    "firstName": "Polina",
+    "surname": "Podrezanova",
+    "age": 19,
+    "score": 4.85
+  },
+  {
+    "id": 6,
+    "firstName": "Ulayna",
+    "surname": "Medved",
+    "age": 18,
+    "score": 5.1
+  },
+  {
+    "id": 7,
+    "firstName": "Katerina",
+    "surname": "Galko",
+    "age": 19,
+    "score": 9.23
+  },
+  {
+    "id": 8,
+    "firstName": "Alexandra",
+    "surname": "Pogonyailo",
+    "age": 19,
+    "score": 6.4
+  },
+  {
+    "id": 9,
+    "firstName": "Alexandr",
+    "surname": "Shima",
+    "age": 19,
+    "score": 4.83
+  },
+  {
+    "id": 10,
+    "firstName": "Dmitry",
+    "surname": "Volodin",
+    "age": 20,
+    "score": 7.85
+  },
+  {
+    "id": 11,
+    "firstName": "Alex",
+    "surname": "Petrov",
+    "age": 18,
+    "score":  8.5
+  }
+]
